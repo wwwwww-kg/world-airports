b81cff18b66c68f5f580c268578eb1de1b409d0c
mma <111668501+mmalkindi@users.noreply.github.com>
2024-11-29 00:19:27 +0700

    Add draft fuzzy search implementation

--- main/views.py
+++ main/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from SPARQLWrapper import SPARQLWrapper, JSON
-from fuzzywuzzy import fuzz, process
+from SPARQLWrapper import SPARQLWrapper2, JSON
+from thefuzz import fuzz, process
 import environ
 
-# Setup SPARQL
+# Setup environment variables
 env = environ.Env()
 environ.Env.read_env()
-sparql = SPARQLWrapper(env("DEPLOYED_GRAPHDB_URL") or "http://localhost:7200/repositories/tk2-airports")
-sparql.setReturnFormat(JSON)
+base_iri = env("BASE_IRI")
 
 # Create your views here.
 def index(request):
@@ -18,19 +17,31 @@ def search(request):
     ''' Menampilkan hasil pencarian '''
 
     query = request.GET.get("q").lower()
-    sparql.setQuery("""
+    local_data_wrapper = SPARQLWrapper2(base_iri)
+
+    ## Get all airports?
+    # TODO Update with actual query
+    local_data_wrapper.setQuery("""
     PREFIX v: <http://world-airports-kg.up.railway.app/data/verb/>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
     select ?airport_name where {
         ?s a [rdfs:label "Airport"]; rdfs:label ?airport_name .
-        FILTER contains(LCASE(?airport_name),"%s") .
-    } limit 100
-    """ % query)
+    }
+    """)
+    raw_results = local_data_wrapper.query().bindings
+
+    ## Filter with fuzzy search
+    MINIMUM_RATIO = 50
+    print(query)
+    weighted_results = process.extract(query, raw_results, scorer=fuzz.token_set_ratio, limit=100)
+    legible_results = []
+    for (result, ratio) in weighted_results:
+        if ratio >= MINIMUM_RATIO:
+            legible_results.append(result)
     
-    results = sparql.queryAndConvert()
     context = {
-        'search_results': results["results"]["bindings"],
+        'search_results': legible_results,
     }
     response = render(request, 'search_results.html', context)
     return response
