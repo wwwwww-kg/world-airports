9a5b81b1eb4d81bc34077b0c515a86290ce80b28
mma <111668501+mmalkindi@users.noreply.github.com>
2024-12-05 19:31:02 +0700

    Fix missing setting error

--- world_airports/settings.py
+++ world_airports/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'world_airports.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
